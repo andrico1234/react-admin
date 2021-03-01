f13e1520ecda5236ce0822f3867daf6d576075d9
HH: Andrico Karoulla <andrico@hhs-macbook-pro.home>
2021-03-01 08:54:18 +0000

    simplify file

--- examples/simple/src/posts/PostEdit.tsx
+++ examples/simple/src/posts/PostEdit.tsx
@@ -4,108 +4,33 @@ import {
     FormTab,
     ReferenceArrayInput,
     AutocompleteArrayInput,
-    FormDataConsumer,
     ArrayInput,
     SimpleFormIterator,
-    SelectInput,
     TabbedForm,
 } from 'react-admin'; // eslint-disable-line import/no-unresolved
 
-const choices = [
-    { id: 'tech', name: 'tech' },
-    { id: 'lifestyle', name: 'lifestyle' },
-];
-
-function DebugField(props) {
-    console.log(props);
-    return null;
-}
-
 // prepopulate with existing data
-const PostEdit = ({ permissions, ...props }) => (
+const PostEdit = props => (
     <Edit {...props}>
-        <TabbedForm initialValues={{ average_note: 0 }} warnWhenUnsavedChanges>
-            {/* <FormTab label="Single">
-        <SelectInput
-          label="category"
-          source="category"
-          allowEmpty
-          choices={choices}
-        />
-
-        <FormDataConsumer>
-          {({ formData, ...rest }) => {
-            if (!formData?.category) {
-              return null;
-            }
-
-            const { category } = formData;
-
-            return (
-              <ReferenceArrayInput
-                label="Comments"
-                source="comments"
-                reference="comments"
-                filter={{ category }}
-              >
-                <AutocompleteArrayInput
-                  source="id"
-                  optionText={(record) => {
-                    if (!record) return null;
-
-                    return record.title;
-                  }}
-                />
-              </ReferenceArrayInput>
-            );
-          }}
-        </FormDataConsumer>
-      </FormTab> */}
+        <TabbedForm>
             <FormTab label="Array">
                 <ArrayInput source="related">
                     <SimpleFormIterator>
-                        <SelectInput
-                            label="category"
-                            source="category"
-                            allowEmpty
-                            choices={choices}
-                        />
+                        <ReferenceArrayInput
+                            //broken when omitted
+                            basePath="/comments"
+                            source="comments"
+                            label="Comments"
+                            reference="comments"
+                        >
+                            <AutocompleteArrayInput
+                                optionText={record => {
+                                    if (!record) return null;
 
-                        <FormDataConsumer>
-                            {({
-                                formData,
-                                scopedFormData,
-                                getSource,
-                                ...rest
-                            }) => {
-                                if (!scopedFormData?.category) {
-                                    return null;
-                                }
-
-                                console.log('res', rest, scopedFormData);
-
-                                const { category } = scopedFormData;
-
-                                return (
-                                    <ReferenceArrayInput
-                                        {...rest}
-                                        // basePath="/comments" broken when omitted
-                                        source={getSource('comments')}
-                                        label="Comments"
-                                        reference="comments"
-                                        filter={{ category }}
-                                    >
-                                        <AutocompleteArrayInput
-                                            optionText={record => {
-                                                if (!record) return null;
-
-                                                return `${record.id}`;
-                                            }}
-                                        />
-                                    </ReferenceArrayInput>
-                                );
-                            }}
-                        </FormDataConsumer>
+                                    return `${record.id}`;
+                                }}
+                            />
+                        </ReferenceArrayInput>
                     </SimpleFormIterator>
                 </ArrayInput>
             </FormTab>
