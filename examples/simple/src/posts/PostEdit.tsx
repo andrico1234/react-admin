c3c572e0b982a52cb9d1f9172b6b67b3afc81bd0
HH: Andrico Karoulla <andrico@hhs-macbook-pro.home>
2021-04-06 10:39:32 +0100

    create repro

--- examples/simple/src/posts/PostEdit.tsx
+++ examples/simple/src/posts/PostEdit.tsx
@@ -1,183 +1,53 @@
-import RichTextInput from 'ra-input-rich-text';
 import * as React from 'react';
 import {
-    TopToolbar,
-    AutocompleteInput,
     ArrayInput,
-    BooleanInput,
-    CheckboxGroupInput,
-    Datagrid,
-    DateField,
-    DateInput,
     Edit,
-    CloneButton,
-    ShowButton,
-    EditButton,
     FormTab,
-    ImageField,
-    ImageInput,
-    NumberInput,
-    ReferenceManyField,
-    ReferenceInput,
-    SelectInput,
     SimpleFormIterator,
-    TabbedForm,
-    TextField,
     TextInput,
-    minValue,
-    number,
-    required,
-    FormDataConsumer,
+    TabbedForm,
 } from 'react-admin'; // eslint-disable-line import/no-unresolved
-import { Box } from '@material-ui/core';
 
-import PostTitle from './PostTitle';
-import TagReferenceInput from './TagReferenceInput';
+function validate(values: any) {
+    const { authors } = values;
+    const errors: any = {
+        authors: [],
+    };
 
-const EditActions = ({ basePath, data, hasShow }) => (
-    <TopToolbar>
-        <CloneButton
-            className="button-clone"
-            basePath={basePath}
-            record={data}
-        />
-        {hasShow && <ShowButton basePath={basePath} record={data} />}
-    </TopToolbar>
-);
+    // works fine
+    errors.singleRole = ['A role already exists with this author.'];
 
-const SanitizedBox = ({ fullWidth, basePath, ...props }) => <Box {...props} />;
+    // this doesn't works
+    if (authors) {
+        authors.forEach((author: any, i: number) => {
+            if (!author) return;
+
+            const error: any = {};
+
+            if (author.role) {
+                error.role = ['A role already exists with this author.'];
+                errors.authors = [];
+                errors.authors[i] = error;
+            }
+        });
+    }
+
+    return errors;
+}
 
 const PostEdit = ({ permissions, ...props }) => (
-    <Edit title={<PostTitle />} actions={<EditActions />} {...props}>
-        <TabbedForm initialValues={{ average_note: 0 }} warnWhenUnsavedChanges>
+    <Edit {...props}>
+        <TabbedForm warnWhenUnsavedChanges validate={validate}>
             <FormTab label="post.form.summary">
-                <SanitizedBox
-                    display="flex"
-                    flexDirection="column"
-                    width="100%"
-                    justifyContent="space-between"
-                    fullWidth
-                >
-                    <TextInput disabled source="id" />
-                    <TextInput
-                        source="title"
-                        validate={required()}
-                        resettable
-                    />
-                </SanitizedBox>
-                <TextInput
-                    multiline={true}
-                    fullWidth={true}
-                    source="teaser"
-                    validate={required()}
-                    resettable
-                />
-                <CheckboxGroupInput
-                    source="notifications"
-                    choices={[
-                        { id: 12, name: 'Ray Hakt' },
-                        { id: 31, name: 'Ann Gullar' },
-                        { id: 42, name: 'Sean Phonee' },
-                    ]}
-                />
-                <ImageInput multiple source="pictures" accept="image/*">
-                    <ImageField source="src" title="title" />
-                </ImageInput>
-                {permissions === 'admin' && (
-                    <ArrayInput source="authors">
-                        <SimpleFormIterator>
-                            <ReferenceInput
-                                label="User"
-                                source="user_id"
-                                reference="users"
-                            >
-                                <AutocompleteInput />
-                            </ReferenceInput>
-                            <FormDataConsumer>
-                                {({
-                                    formData,
-                                    scopedFormData,
-                                    getSource,
-                                    ...rest
-                                }) =>
-                                    scopedFormData && scopedFormData.user_id ? (
-                                        <SelectInput
-                                            label="Role"
-                                            source={getSource('role')}
-                                            choices={[
-                                                {
-                                                    id: 'headwriter',
-                                                    name: 'Head Writer',
-                                                },
-                                                {
-                                                    id: 'proofreader',
-                                                    name: 'Proof reader',
-                                                },
-                                                {
-                                                    id: 'cowriter',
-                                                    name: 'Co-Writer',
-                                                },
-                                            ]}
-                                            {...rest}
-                                        />
-                                    ) : null
-                                }
-                            </FormDataConsumer>
-                        </SimpleFormIterator>
-                    </ArrayInput>
-                )}
-            </FormTab>
-            <FormTab label="post.form.body">
-                <RichTextInput
-                    source="body"
-                    label=""
-                    validate={required()}
-                    addLabel={false}
-                />
-            </FormTab>
-            <FormTab label="post.form.miscellaneous">
-                <TagReferenceInput
-                    reference="tags"
-                    source="tags"
-                    label="Tags"
-                />
-                <ArrayInput source="backlinks">
+                {/* works fine */}
+                <TextInput source="singleRole" />
+
+                {/* doesn't work */}
+                <ArrayInput source="authors">
                     <SimpleFormIterator>
-                        <DateInput source="date" />
-                        <TextInput source="url" validate={required()} />
+                        <TextInput source="role" />
                     </SimpleFormIterator>
                 </ArrayInput>
-                <DateInput source="published_at" options={{ locale: 'pt' }} />
-                <SelectInput
-                    allowEmpty
-                    resettable
-                    source="category"
-                    choices={[
-                        { name: 'Tech', id: 'tech' },
-                        { name: 'Lifestyle', id: 'lifestyle' },
-                    ]}
-                />
-                <NumberInput
-                    source="average_note"
-                    validate={[required(), number(), minValue(0)]}
-                />
-                <BooleanInput source="commentable" defaultValue />
-                <TextInput disabled source="views" />
-            </FormTab>
-            <FormTab label="post.form.comments">
-                <ReferenceManyField
-                    reference="comments"
-                    target="post_id"
-                    addLabel={false}
-                    fullWidth
-                >
-                    <Datagrid>
-                        <DateField source="created_at" />
-                        <TextField source="author.name" />
-                        <TextField source="body" />
-                        <EditButton />
-                    </Datagrid>
-                </ReferenceManyField>
             </FormTab>
         </TabbedForm>
     </Edit>
